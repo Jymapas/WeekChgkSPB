0f1e0ec637de67c3edaaca1c800ed49ae0b83aa6
Jymapas <jymapas@yandex.ru>
2025-08-01 14:34:14 +0500

    chore: create csproj

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿var httpClient = new HttpClient();
+var response = await httpClient.GetAsync("https://chgk-spb.livejournal.com/data/rss");
+var r = await response.Content.ReadAsStringAsync();
+Console.WriteLine(r);
+Console.ReadKey();
