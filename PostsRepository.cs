37ff032f033e4b68afd455211074b3d1dd8e4c82
Jymapas <jymapas@yandex.ru>
2025-08-01 15:28:51 +0500

    chore(PostsRepository): add Exists and Insert methods

--- PostsRepository.cs
+++ PostsRepository.cs
@@ -26,4 +26,28 @@ public class PostsRepository
             )";
         cmd.ExecuteNonQuery();
     }
+
+    public bool Exists(int id)
+    {
+        using var connection = new SqliteConnection($"Data Source={_dbPath}");
+        connection.Open();
+        var cmd = connection.CreateCommand();
+        cmd.CommandText = @"SELECT COUNT(*) FROM posts WHERE id=@id";
+        cmd.Parameters.AddWithValue("@id", id);
+        var count = (int)cmd.ExecuteScalar()!;
+        return count > 0;
+    }
+
+    public void Insert(Post post)
+    {
+        using var connection = new SqliteConnection($"Data Source={_dbPath}");
+        connection.Open();
+        var cmd = connection.CreateCommand();
+        cmd.CommandText = "INSERT INTO posts (id, title, link, description) VALUES (@id, @title, @link, @description)";
+        cmd.Parameters.AddWithValue("@id", post.Id);
+        cmd.Parameters.AddWithValue("@title", post.Title);
+        cmd.Parameters.AddWithValue("@link", post.Link);
+        cmd.Parameters.AddWithValue("@description", post.Description);
+        cmd.ExecuteNonQuery();
+    }
 }
