87a5250620f7e54062b48e0c91fa20c178ff48c4
Jymapas <jymapas@yandex.ru>
2025-08-11 00:42:14 +0500

    feat(Bot): add AddAnnouncementState

---
+++ Infrastructure/Bot/AddAnnouncementState.cs
@@ -0,0 +1,18 @@
+﻿namespace WeekChgkSPB.Infrastructure.Bot;
+
+internal enum AddStep
+{
+    None,
+    WaitingId,
+    WaitingName,
+    WaitingPlace,
+    WaitingDateTime,
+    WaitingCost,
+    Done
+}
+
+internal class AddAnnouncementState
+{
+    public AddStep Step { get; set; } = AddStep.None;
+    public Announcement Draft { get; } = new();
+}
