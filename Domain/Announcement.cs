852f5330b8b2b014bd088baf4af6417894142096
Jymapas <jymapas@yandex.ru>
2025-08-11 00:31:04 +0500

    feat(Domain): add Announcement

---
+++ Domain/Announcement.cs
@@ -0,0 +1,10 @@
+﻿namespace WeekChgkSPB;
+
+public class Announcement
+{
+    public long Id { get; set; }
+    public string TournamentName { get; set; } = "";
+    public string Place { get; set; } = "";
+    public DateTime DateTimeUtc { get; set; }
+    public int Cost { get; set; }
+}
