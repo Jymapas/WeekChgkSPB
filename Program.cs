6845aadfd9730accc918eb4151a5c6453ff367b7
Aleksandr Petrakov <jymapas@yandex.ru>
2025-09-30 22:01:43 +0500

    chore: extract ServiceCollectionExtensions

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ using WeekChgkSPB.Infrastructure.Bot;
 using WeekChgkSPB.Infrastructure.Bot.Commands;
 using WeekChgkSPB.Infrastructure.Bot.Flows;
 using WeekChgkSPB.Infrastructure.Notifications;
+using WeekChgkSPB.Infrastructure.Configuration;
 
 internal class Program
 {
@@ -23,7 +24,9 @@ internal class Program
             return;
         }
 
-        using var host = BuildHost(settings);
+        using var host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services => services.AddWeekChgkSpbServices(settings, RssUrl))
+            .Build();
 
         using var scope = host.Services.CreateScope();
         var services = scope.ServiceProvider;
@@ -182,81 +185,6 @@ internal class Program
         return true;
     }
 
-    private static IHost BuildHost(AppSettings settings)
-    {
-        return Host.CreateDefaultBuilder()
-            .ConfigureServices(services => ConfigureServices(services, settings))
-            .Build();
-    }
-
-    private static void ConfigureServices(IServiceCollection services, AppSettings settings)
-    {
-        services.AddSingleton(new PostsRepository(settings.DbPath));
-        services.AddSingleton(new FootersRepository(settings.DbPath));
-        services.AddSingleton(new AnnouncementsRepository(settings.DbPath));
-        services.AddSingleton(new ChannelPostsRepository(settings.DbPath));
-        services.AddSingleton(new RssFetcher(RssUrl));
-        services.AddSingleton<INotifier>(_ => new TelegramNotifier(settings.BotToken, settings.ChatId));
-        services.AddSingleton(_ => new BotCommandHelper(PostFormatter.Moscow));
-        services.AddSingleton<BotConversationState>();
-        services.AddSingleton<IConversationFlowHandler, AddAnnouncementFlow>();
-        services.AddSingleton<IConversationFlowHandler, EditAnnouncementFlow>();
-        services.AddSingleton<IConversationFlowHandler, FooterFlow>();
-        services.AddSingleton<IBotCommandHandler>(sp => new MakePostCommandHandler(BotCommands.MakePostLJ, true));
-        services.AddSingleton<IBotCommandHandler>(sp => new MakePostCommandHandler(BotCommands.MakePost, false));
-        services.AddSingleton<IBotCommandHandler, AddLinesCommandHandler>();
-        services.AddSingleton<IBotCommandHandler, AddCommandHandler>();
-        services.AddSingleton<IBotCommandHandler, EditNameCommandHandler>();
-        services.AddSingleton<IBotCommandHandler, EditPlaceCommandHandler>();
-        services.AddSingleton<IBotCommandHandler, EditDateTimeCommandHandler>();
-        services.AddSingleton<IBotCommandHandler, EditCostCommandHandler>();
-        services.AddSingleton<IBotCommandHandler, EditCommandHandler>();
-        services.AddSingleton<IBotCommandHandler, DeleteAnnouncementCommandHandler>();
-        services.AddSingleton<IBotCommandHandler, FooterAddCommandHandler>();
-        services.AddSingleton<IBotCommandHandler, FooterListCommandHandler>();
-        services.AddSingleton<IBotCommandHandler, FooterDeleteCommandHandler>();
-        services.AddSingleton<ITelegramBotClient>(_ => new TelegramBotClient(settings.BotToken));
-        services.AddSingleton<IChannelPostUpdater>(sp =>
-        {
-            if (!settings.HasChannel)
-            {
-                return new NoOpChannelPostUpdater();
-            }
-
-            return new ChannelPostUpdater(
-                sp.GetRequiredService<AnnouncementsRepository>(),
-                sp.GetRequiredService<FootersRepository>(),
-                sp.GetRequiredService<ChannelPostsRepository>(),
-                sp.GetRequiredService<ITelegramBotClient>(),
-                settings.ChannelId!);
-        });
-        services.AddSingleton(sp => new BotRunner(
-            sp.GetRequiredService<ITelegramBotClient>(),
-            settings.ChatId,
-            sp.GetRequiredService<PostsRepository>(),
-            sp.GetRequiredService<AnnouncementsRepository>(),
-            sp.GetRequiredService<FootersRepository>(),
-            sp.GetRequiredService<BotCommandHelper>(),
-            sp.GetRequiredService<BotConversationState>(),
-            sp.GetServices<IBotCommandHandler>(),
-            sp.GetServices<IConversationFlowHandler>()));
-
-        if (settings.HasScheduler)
-        {
-            var options = settings.ScheduleOptions!;
-            var channelId = settings.ChannelId!;
-            services.AddSingleton(options);
-            services.AddSingleton(sp => new ScheduledPostPublisher(
-                sp.GetRequiredService<AnnouncementsRepository>(),
-                sp.GetRequiredService<FootersRepository>(),
-                sp.GetRequiredService<ChannelPostsRepository>(),
-                sp.GetRequiredService<ITelegramBotClient>(),
-                channelId,
-                options,
-                TimeZoneInfo.Local));
-        }
-    }
-
     private static string ResolveDbPath(string? envPath, string baseDir)
     {
         if (string.IsNullOrWhiteSpace(envPath))
@@ -275,14 +203,4 @@ internal class Program
 
     }
 
-    private sealed record AppSettings(
-        string DbPath,
-        string BotToken,
-        long ChatId,
-        string? ChannelId,
-        ChannelPostScheduleOptions? ScheduleOptions)
-    {
-        public bool HasChannel => !string.IsNullOrEmpty(ChannelId);
-        public bool HasScheduler => HasChannel && ScheduleOptions is not null;
-    }
 }
