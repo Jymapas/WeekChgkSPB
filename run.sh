c1d7dda3d729f70e26123b5b7108ed0a47f06ec3
Aleksandr Petrakov <a.petrakov@tha.kz>
2026-01-04 14:04:17 +0500

    chore(run.sh): turn back ping telegram api

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Waiting for network..."
 
-while ! ping -c1 8.8.8.8 >/dev/null 2>&1; do
+while ! ping -c1 api.telegram.org >/dev/null 2>&1; do
   echo "No network yet, retrying in 10s..."
   sleep 10
 done
